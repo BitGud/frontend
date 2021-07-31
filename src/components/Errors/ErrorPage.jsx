e41e7c6ea3a6297ce44863647a27f50188dd2a65
LittleBob881 <68731508+LittleBob881@users.noreply.github.com>
2021-07-31 16:05:53 +1200

    Error component basic structure

--- src/components/Errors/ErrorPage.jsx
+++ src/components/Errors/ErrorPage.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
+import { Grid, Switch } from '@material-ui/core'
 
 import './Errors.scss'
 
-import { icons } from '../../utils'
+import icons from '../../utils/icons'
 
 const ErrorPage = (props) => {
   return (
     <div className="messageContainer">
-      <h1 className="notFoundText">Oh no, the router cannot be found</h1>
+      {/* Switch?? */}
+      <Switch defaultChecked color="default" inputProps={{ 'aria-label': 'checkbox with default color' }} />
+      <h1 className="notFoundText">Uh oh!</h1>
+      <h1 className="notFoundTextLine2">The page cannot be found...</h1>
+      <Grid container>
+        <Grid item>
+          <img alt="error" src={icons.error} />
+        </Grid>
+        <Grid item>
+          <img alt="error 2" src={icons.error2} />
+        </Grid>
+      </Grid>
     </div>
   )
 }
--- src/utils/icons.js
+++ src/utils/icons.js
@@ -1,5 +1,7 @@
 import login from '../assets/icons/login.png'
 import login2 from '../assets/icons/login2.png'
 import login3 from '../assets/icons/login3.png'
+import error from '../assets/icons/error.png'
+import error2 from '../assets/icons/error2.png'
 
-export default { login, login2, login3 }
+export default { login, login2, login3, error, error2 }
