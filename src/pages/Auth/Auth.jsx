7da31ae963700cde7302e1684ce83930569ec6b2
LittleBob881 <68731508+LittleBob881@users.noreply.github.com>
2021-07-31 15:53:34 +1200

    Auth page basic structure

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,12 +1,12 @@
-import { Grid } from '@material-ui/core'
 import React from 'react'
+import { Grid, Switch } from '@material-ui/core'
 
 import icons from '../../utils/icons'
 
 function Auth(props) {
   return (
     <div>
-      <button type="button">Slider</button>
+      <Switch defaultChecked color="default" inputProps={{ 'aria-label': 'checkbox with default color' }} />
       <button type="button">Register</button>
       <div>LOGIN - BUM BUM BUM</div>
 
@@ -21,6 +21,22 @@ function Auth(props) {
           <input />
         </Grid>
       </Grid>
+      <button type="button">Okay, go!</button>
+
+      <img alt="login 2" src={icons.login2} />
+      <button type="button">Login</button>
+      <div>REGISTER HERE ^^</div>
+
+      {/* In a Grid?? */}
+      <div>Your name here</div>
+      <input />
+      <div>Your password here</div>
+      <input />
+      <div>Confirm your password</div>
+      <input />
+      <button type="button">Okay, go!</button>
+
+      <img alt="login 3" src={icons.login3} />
     </div>
   )
 }
--- src/utils/icons.js
+++ src/utils/icons.js
@@ -1,3 +1,5 @@
 import login from '../assets/icons/login.png'
+import login2 from '../assets/icons/login2.png'
+import login3 from '../assets/icons/login3.png'
 
-export default { login }
+export default { login, login2, login3 }
