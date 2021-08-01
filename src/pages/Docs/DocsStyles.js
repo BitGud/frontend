d5efffb5b2c6afa954b9239cde1fd64bc72a5ca2
LittleBob881 <68731508+LittleBob881@users.noreply.github.com>
2021-08-01 13:57:41 +1200

    Docs formatting and started

---
+++ src/pages/Docs/DocsStyles.js
@@ -0,0 +1,34 @@
+import { makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+  docsContentsHeadingText: {
+    fontSize: '2em',
+    fontFamily: 'Share',
+    textAlign: 'center',
+    margin: 'auto',
+  },
+  docsContentsText: {
+    fontSize: '1.5em',
+    fontFamily: 'Share',
+    textAlign: 'center',
+    margin: 'auto',
+  },
+  docsBodyHeadingText: {
+    fontSize: '3em',
+    fontFamily: 'Share',
+    textAlign: 'center',
+    margin: 'auto',
+  },
+  docsBodySubheadingText: {
+    fontSize: '2em',
+    fontFamily: 'Share',
+    margin: 'auto',
+  },
+  docsBodyText: {
+    fontSize: '1.2em',
+    fontFamily: 'Share',
+    margin: 'auto',
+  },
+}))
+
+export default useStyles
